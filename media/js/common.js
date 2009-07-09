43d943e05ac043b2d8f3fd57eb8d5d4c6880cbbc
indigo.sunrise <indigo.sunrise@e2e9d778-6c14-11de-a36e-1937e544de9f>
2009-07-09 00:07:08 +0000

    Changed all references from Anfiniti RPG into the new name, Crindigan. Also removed a useless hello_index template.

--- media/js/common.js
+++ media/js/common.js
@@ -1,20 +1,20 @@
 /**
- * This file is part of Anfiniti RPG.
+ * This file is part of Crindigan.
  *
- * Anfiniti RPG is free software: you can redistribute it and/or modify
+ * Crindigan is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Anfiniti RPG is distributed in the hope that it will be useful,
+ * Crindigan is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Anfiniti RPG. If not, see <http://www.gnu.org/licenses/>.
+ * along with Crindigan. If not, see <http://www.gnu.org/licenses/>.
  *
- * @package   AnfinitiRPG
+ * @package   Crindigan
  * @version   $Revision$
  * @copyright Copyright (c) 2009 Steven Harris
  * @license   http://www.gnu.org/licenses/gpl.txt GPL
